aad82a889873a2d85fc5dcc0cbe7c1c23d9b8b57
Dimitri Nouveau <dimitri.nouveau@gmail.com>
2014-10-26 18:15:57 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
 ## Altogether, these functions allow the user to cache the inverse
 ## of a sqaure, non-singular matrix.
 
-## The first function takes a matrix as an argument and returns four functions
+## The first function takes a matrix as an argument and returns a list of four functions
 ## which will then be used in the cache function.
 
 makeCacheMatrix <- function(x = matrix()) {
-        ##checks whether the matrix has an inverse
+        ##checks whether the matrix has an inverse with two conditions
         if(rcond(x) < 1e-12) {
                 message("Your matrix is singular. Please change values")
         }      
@@ -13,12 +13,14 @@ makeCacheMatrix <- function(x = matrix()) {
                 message("Your matrix is not square. Please change values")
         }
         else { 
+                ##sets the inverse of the matrix to 0 in case get is called
+                ##before I is computed with cacheSolve or set
                 I <- NULL
                 set <- function(y = matrix()) {
                         ##sets x and I in the parent frame
                         x <<- y
                         I <<- NULL
-                        ##checks whether the matrix has an inverse
+                        ##checks whether the matrix defined with set has an inverse
                         if(rcond(y) < 1e-12) {
                                 message("your matrix is singular. Please change values")
                         }
@@ -26,16 +28,19 @@ makeCacheMatrix <- function(x = matrix()) {
                                 message("Your matrix is not square. Please change values")
                         }
                 }
-                ##list of four functions to be used in cacheSolve
+                ##get returns the matrix
                 get <- function() x
+                ##setinverse sets the inverse in the parent frame
                 setinverse <- function(inverse) I <<- inverse
+                #getinverse returns the inverse of the matrix
                 getinverse <- function() I
+                ##the function makeCacheMatrix returns a list of four functions
                 list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
         }
 }
 
 ## This function uses the "special" matrix created above to compute and cache
-## the inverse of a matrix
+## its inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -49,5 +54,6 @@ cacheSolve <- function(x, ...) {
         data <- x$get()
         I <- solve(data,...)
         x$setinverse(I)
+        ##returns the inverse of 'x'
         I
 }
